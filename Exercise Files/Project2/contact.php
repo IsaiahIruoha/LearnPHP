cfd9d637428798fda820ec8f8724638822f16512
Isaiah Iruoha <76797894+IsaiahIruoha@users.noreply.github.com>
2023-07-31 01:13:36 -0400

    Add files via upload

---
+++ Exercise Files/Project2/contact.php
@@ -0,0 +1,6 @@
+<?php include 'templates/header.php'; ?>
+<!--Main Content Start-->
+            <h2>Contact <?php echo $author; ?></h2>
+            <p>Email me IsaiahIruoha [at] gmail.com</p>
+<!--End Main Content-->
+<?php include 'templates/footer.php'; ?>
